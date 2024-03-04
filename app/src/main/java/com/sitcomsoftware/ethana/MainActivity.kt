73deb973590601988a78cec2b6fe4d72a15cf433
PlasmaMWK <72070358+PlasmaMWK@users.noreply.github.com>
2024-03-04 11:53:35 +0100

    Fix probleme bandeau blanc + centrage des éléments

--- app/src/main/java/com/sitcomsoftware/ethana/MainActivity.kt
+++ app/src/main/java/com/sitcomsoftware/ethana/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sitcomsoftware.ethana
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        actionBar?.hide();
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -22,6 +27,10 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        if (getSupportActionBar() != null) {
+            getSupportActionBar()?.setBackgroundDrawable(getResources().getDrawable(R.color.custom_button_color));
+        }
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_form_search
